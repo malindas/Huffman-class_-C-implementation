bee5103b108d2f38c110622d21b5919f5ebe96f7
Malinda Sulochana <34549024+malindas@users.noreply.github.com>
2018-03-22 23:31:53 +0530

    Add files via upload
    Initial commit

---
+++ Huffman.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+template<class sym, class prob>
+class Huffman {
+
+public:
+	map<sym, bool> huffmandict(sym * symbol, prob * probability)
+
+private:
+
+
+
+};
+
+template<class sym, class prob>
+inline map<sym, bool> huffmandict(sym * symobl, prob * probability) {
+
+}
